80d28bf7422b7c85414d40d5e34d0a65eb390259
Reinhard Stahn <rainij36@proton.me>
2026-09-04 11:26:07 +0000

    Update llvm version and mqt core commit

--- mlir/cmake/ExternalDependencies.cmake
+++ mlir/cmake/ExternalDependencies.cmake
@@ -4,7 +4,7 @@ set(FETCH_PACKAGES "")
 FetchContent_Declare(
   mqt-core
   GIT_REPOSITORY https://github.com/munich-quantum-toolkit/core.git
-  GIT_TAG 25b7eb8ef50db57abe0de94044821a160f384b89 # Date:   Fri Aug 28 00:41:57 2026 +0200
+  GIT_TAG efbba8902710cebae60e865ef031df146a5868bf # Date:   Fri Sep 4 12:42:26 2026 +0200
 )
 list(APPEND FETCH_PACKAGES mqt-core)
 
