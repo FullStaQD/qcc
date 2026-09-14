56b4899d8c7d029b30bd8ddb6ac0a697ed8c8e13
Reinhard Stahn <rainij36@proton.me>
2026-09-14 14:48:49 +0200

    Update mlir/include/qcc/Dialect/QVec/IR/QVec.h
    Co-authored-by: Julian Farnsteiner <jcf96@proton.me>
    Signed-off-by: Reinhard Stahn <rainij36@proton.me>

--- mlir/include/qcc/Dialect/QVec/IR/QVec.h
+++ mlir/include/qcc/Dialect/QVec/IR/QVec.h
@@ -67,7 +67,7 @@ inline bool isQubitVector(mlir::Type type) {
 
 namespace qcc::qvec {
 
-/// Trace back the qubit at `index` in the vector `qubits` to a StaticOp and return it if possible (null value if not).
+/// Traces back the qubit at `index` in the vector `qubits` to a StaticOp and returns it if possible (null value if not).
 mlir::qco::StaticOp getStaticOpAncestor(mlir::TypedValue<mlir::VectorType> qubits, int64_t index);
 
 /// Adds the `qvec` (lane) operations that produced any element of `qubits` to `producers`.
